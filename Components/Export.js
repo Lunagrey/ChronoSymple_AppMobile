2b721cecaa3074328aea8c54255d26c0457acbbd
victor <victor.zhu@epitech.eu>
2019-01-21 16:52:44 +0100

    [MODIFICATION] page statictic + page export

--- Components/Export.js
+++ Components/Export.js
@@ -1,13 +1,30 @@
 
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Button, Dimensions } from 'react-native'
 
 export default class Export extends React.Component {
 
   render() {
+    let deviceWidth = Dimensions.get('window').width
     return (
       <View style={styles.container}>
-        <Text>Screen 1</Text>
+        <Text style={{ fontSize: 20 }}>partager avec mon docteur{'\n'}</Text>
+        
+        <Button 
+          color="#62BE87"
+          style={{width: deviceWidth / 2, borderRadius: 15}}
+          onPress={() => {}} 
+          title="Choisir le docteur"
+        />
+        <Text>{'\n\n'}</Text>
+
+        <Text style={{ fontSize: 20 }}>Télécharger sous format PDF{'\n'}</Text>
+        <Button 
+          color="#62BE87"
+          style={{width: deviceWidth / 2, borderRadius: 15}}
+          onPress={() => {}} 
+          title="Télécharger"
+        />
       </View>
     )
   }
@@ -16,8 +33,9 @@ export default class Export extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
   },
+
 })
--- Components/Statistic.js
+++ Components/Statistic.js
@@ -1,13 +1,27 @@
 // Components/Statistic.js
 
 import React from 'react'
-import { View, Text} from 'react-native'
+import { View, Text, Dimensions } from 'react-native'
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
 
 class Statistic extends React.Component {
   render() {
+  	let deviceWidth = Dimensions.get('window').width
+  	const data = [ 50, 10, 40, 95, -4, -24, 85 ]
+  	// faudra remplacer la data par ce que renverra la BDD
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ flex: 1 }} >JE SUIS LA STATISTIC</Text>
+      <View style={{ flex: 1, justifyContent: "center"/*, alignItems: "center"*/ }}>
+        <Text style={{ marginLeft: deviceWidth / 10 * 4}}>STATISTIQUE</Text>
+        <AreaChart
+            style={{ height: 200, margin: 20, borderWidth: 1}}
+            data={ data }
+            contentInset={{ top: 30, bottom: 30 }}
+            curve={ shape.curveNatural }
+            svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+        >
+            <Grid/>
+        </AreaChart>
       </View>
     )
   }
